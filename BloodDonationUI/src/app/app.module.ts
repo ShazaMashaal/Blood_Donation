e7d1f19265b286e6a5ca0f8a3e36fbb7632580cc
Shaza <shazamashaal308@gmail.com>
2021-08-13 03:05:02 +0200

    try to route from home to register

--- BloodDonationUI/src/app/app.module.ts
+++ BloodDonationUI/src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {APP_BASE_HREF} from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent,
+
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: "",
+    redirectTo: '/first',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    HomePageComponent,
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(routes)],
   ],
-  providers: [],
+  exports: [RouterModule],
+
+
+  providers: [
+    {provide: APP_BASE_HREF, useValue : '/' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
